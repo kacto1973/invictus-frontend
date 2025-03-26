44ca8fc4f1a5eb80ba9f2c9d5f431ce3152016ca
kacto1973 <a222212359@unison.mx>
2025-03-26 16:32:38 -0700

    xa

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div className="bg-gray- w-screen h-screen">Dashboard</div>;
+  return <div className="bg-[#EDEDED] w-screen h-screen">Dashboard</div>;
 };
 
 export default Dashboard;
