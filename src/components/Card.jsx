e147cbd9c741952d2163604f1e13e38159243761
kacto1973 <a222212359@unison.mx>
2025-03-27 17:11:44 -0700

    j

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Card = ({ icon, label, children }) => {
+const Card = ({ icon, label, children, classNames }) => {
   return (
-    <div className="bg-black rounded-md shadow-md p-4 flex items-center justify-center w-[200px] h-[200px] m-4">
-      <div className="absolute w-full h-[5rem] top-0 left-0 bg-[#E2BDFD]"></div>
+    <div
+      className={`bg-white relative z-50 rounded-md shadow-md p-4 flex items-center justify-center w-[25%] h-[200px] m-4 ${classNames}`}
+    >
+      <div className="absolute w-full  rounded-t-md h-[3.5rem] top-0 left-0 bg-[#E2BDFD] flex items-center">
+        <img src={icon} alt="icon" width={25} className=" ml-4" />
+        <h1 className="text-[#4B2E83] text-lg font-bold ml-2">{label}</h1>
+      </div>
     </div>
   );
 };
