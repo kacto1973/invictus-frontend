c35f5c2637577e7613ee82e02131c1e0d0c3174d
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2025-04-02 18:16:04 -0700

    commit

---
+++ src/components/EquipmentTable.jsx
@@ -0,0 +1,270 @@
+import * as React from "react";
+import { rgbToHex, styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#C796EB",
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+/*dummy data */
+const reactants = [
+  {
+    id: "R001",
+    gabinete: "A1",
+    marca: "Sigma",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Ácido Sulfúrico",
+    esPeligroso: true,
+    cantidad: 500,
+  },
+  {
+    id: "R002",
+    gabinete: "B2",
+    marca: "Merck",
+    unidadMedida: "g",
+    estadoFisico: "Sólido",
+    nombre: "Sulfato de Cobre",
+    esPeligroso: false,
+    cantidad: 250,
+  },
+  {
+    id: "R003",
+    gabinete: "C3",
+    marca: "Fisher",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Ethanol",
+    esPeligroso: true,
+    cantidad: 1000,
+  },
+  {
+    id: "R004",
+    gabinete: "D4",
+    marca: "PanReac",
+    unidadMedida: "g",
+    estadoFisico: "Polvo",
+    nombre: "Nitrato de Plata",
+    esPeligroso: true,
+    cantidad: 100,
+  },
+  {
+    id: "R005",
+    gabinete: "E5",
+    marca: "J.T. Baker",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Acetona",
+    esPeligroso: true,
+    cantidad: 750,
+  },
+  {
+    id: "R006",
+    gabinete: "F6",
+    marca: "Sigma",
+    unidadMedida: "g",
+    estadoFisico: "Sólido",
+    nombre: "Cloruro de Sodio",
+    esPeligroso: false,
+    cantidad: 500,
+  },
+  {
+    id: "R007",
+    gabinete: "G7",
+    marca: "Merck",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Ácido Clorhídrico",
+    esPeligroso: true,
+    cantidad: 1000,
+  },
+  {
+    id: "R008",
+    gabinete: "H8",
+    marca: "Fisher",
+    unidadMedida: "g",
+    estadoFisico: "Sólido",
+    nombre: "Óxido de Calcio",
+    esPeligroso: false,
+    cantidad: 300,
+  },
+  {
+    id: "R009",
+    gabinete: "I9",
+    marca: "PanReac",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Glicerina",
+    esPeligroso: false,
+    cantidad: 500,
+  },
+  {
+    id: "R010",
+    gabinete: "J10",
+    marca: "J.T. Baker",
+    unidadMedida: "g",
+    estadoFisico: "Polvo",
+    nombre: "Óxido de Zinc",
+    esPeligroso: false,
+    cantidad: 400,
+  },
+  {
+    id: "R011",
+    gabinete: "K11",
+    marca: "Sigma",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Tolueno",
+    esPeligroso: true,
+    cantidad: 600,
+  },
+  {
+    id: "R012",
+    gabinete: "L12",
+    marca: "Merck",
+    unidadMedida: "g",
+    estadoFisico: "Sólido",
+    nombre: "Carbonato de Sodio",
+    esPeligroso: false,
+    cantidad: 700,
+  },
+  {
+    id: "R013",
+    gabinete: "M13",
+    marca: "Fisher",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Metanol",
+    esPeligroso: true,
+    cantidad: 850,
+  },
+  {
+    id: "R014",
+    gabinete: "N14",
+    marca: "PanReac",
+    unidadMedida: "g",
+    estadoFisico: "Polvo",
+    nombre: "Sulfato de Aluminio",
+    esPeligroso: false,
+    cantidad: 500,
+  },
+  {
+    id: "R015",
+    gabinete: "O15",
+    marca: "J.T. Baker",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Benceno",
+    esPeligroso: true,
+    cantidad: 450,
+  },
+  {
+    id: "R016",
+    gabinete: "P16",
+    marca: "Sigma",
+    unidadMedida: "g",
+    estadoFisico: "Sólido",
+    nombre: "Óxido de Hierro",
+    esPeligroso: false,
+    cantidad: 550,
+  },
+  {
+    id: "R017",
+    gabinete: "Q17",
+    marca: "Merck",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Cloroformo",
+    esPeligroso: true,
+    cantidad: 300,
+  },
+  {
+    id: "R018",
+    gabinete: "R18",
+    marca: "Fisher",
+    unidadMedida: "g",
+    estadoFisico: "Polvo",
+    nombre: "Fosfato de Calcio",
+    esPeligroso: false,
+    cantidad: 200,
+  },
+  {
+    id: "R019",
+    gabinete: "S19",
+    marca: "PanReac",
+    unidadMedida: "ml",
+    estadoFisico: "Líquido",
+    nombre: "Ácido Nítrico",
+    esPeligroso: true,
+    cantidad: 700,
+  },
+  {
+    id: "R020",
+    gabinete: "T20",
+    marca: "J.T. Baker",
+    unidadMedida: "g",
+    estadoFisico: "Sólido",
+    nombre: "Permanganato de Potasio",
+    esPeligroso: true,
+    cantidad: 250,
+  },
+];
+
+function openDetails(reactant) {
+  console.log(reactant);
+}
+
+export default function CustomizedTables({ onReactantClick }) {
+  return (
+    <TableContainer component={Paper}>
+    <Table sx={{ minWidth: 300 }} aria-label="customized table">
+      <TableHead>
+        <TableRow>
+          <StyledTableCell>Nombre</StyledTableCell>
+          <StyledTableCell>Gabinete</StyledTableCell>
+          <StyledTableCell>Información</StyledTableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {reactants.map((reactant) => (
+          <StyledTableRow key={reactant.nombre}>
+            <StyledTableCell>{reactant.nombre}</StyledTableCell>
+            <StyledTableCell>{reactant.gabinete}</StyledTableCell>
+            <StyledTableCell>
+              <img
+                src="svgs/details.svg"
+                alt="icon"
+                width={25}
+                className="m-auto cursor-pointer"
+                onClick={() => onReactantClick(reactant)}
+              />
+            </StyledTableCell>
+          </StyledTableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+  );
+}
--- src/views/Equipment.jsx
+++ src/views/Equipment.jsx
@@ -4,16 +4,55 @@ import TemporaryDrawer from "../components/TemporaryDrawer";
 import Card from "../components/Card";
 import SearchBox from "../components/SearchBox.jsx";
 import Button from "../components/Button";
+import { useState } from "react";
 
 const Equipment = () => {
+
+ 
+   
+
+    const equiposLaboratorio = [
+      {
+        nombre: "Microscopio Óptico",
+        descripcion: "Permite observar muestras biológicas con aumentos de hasta 1000x.",
+        requiereMantenimiento: true,
+        status: "En Utilización"
+      },
+      {
+        nombre: "Centrífuga de Laboratorio",
+        descripcion: "Utilizada para separar componentes de una muestra por densidad.",
+        requiereMantenimiento: false,
+        status: "Liberado"
+      },
+      {
+        nombre: "Espectrofotómetro",
+        descripcion: "Mide la absorción de luz en diferentes longitudes de onda.",
+        requiereMantenimiento: true,
+        status: "En Utilización"
+      },
+      {
+        nombre: "Balanza Analítica",
+        descripcion: "Permite medir masas con gran precisión hasta microgramos.",
+        requiereMantenimiento: false,
+        status: "Liberado"
+      },
+      {
+        nombre: "Autoclave",
+        descripcion: "Esteriliza material de laboratorio usando vapor de alta presión.",
+        requiereMantenimiento: true,
+        status: "En Utilización"
+      }
+    ];
+        
+  
   return (
     <div className="bg-[#EDEDED] w-screen h-screen relative m-0 overflow-hidden">
       <TemporaryDrawer></TemporaryDrawer>
       <Header label="Equipment"></Header>
       <div
         className={`ml-[250px] mt-[5rem] w-[calc(100vw-250px)]
-           h-[calc(100vh-5rem)] bg-[#EDEDED] overflow-hidden`} >{/* this is pushing everything else below the header */}
-          <div className="flex flex-row w-[100%] h-[12%] ml-5 mt-5 ">
+           h-[calc(100vh-5rem)] bg-[#EDEDED] overflow-hidden p-5`} >{/* this is pushing everything else below the header */}
+          <div className="flex flex-row w-[100%] h-[12%]">
            <SearchBox classNames="w-[400px] h-[3rem] mb-5" />
             <Button 
             icon = "/svgs/plus-sign.svg"
@@ -28,9 +67,26 @@ const Equipment = () => {
             classNames="hover:bg-[#CA5E5E] bg-[#E96D6D]  w-[20%] h-[3rem] mb-5 ml-15"
           />
          </div>
+         
+         <div className="w-[40%] h-[80%] mt-5 flex   ">
+            <div className="relative w-full h-full bg-white shadow-md rounded-md overflow-hidden">
+              <div className="absolute bg-[#E0C8F2] w-full h-[3rem] flex items-center "><p className="ml-5 *:">Equipo de Laboratorio</p></div>
+              <div className="w-full mt-[3rem] h-[calc(100%-5rem)] flex flex-col items-center overflow-y-auto">
+              {equiposLaboratorio.map((equipo, index) => (
+                <div className="w-[80%]  bg-white shadow-md border-2     rounded-md relative mt-4 mb-2 ">
+                      <img className="" width={130} src="images/machine-sample.png" alt="machine image" />
+                      <img onClick={() => { console.log("Execute Task...")}} className="absolute top-2 right-2" src="svgs/arrow-right.svg" width={25} alt="arrow svg" />
+                      <span className=" absolute top-2 left-1/2 -translate-x-[2rem]">{equipo.nombre}</span>
+                      <span className=" absolute bottom-4 right-4 bg-pink-300 rounded-full py-1 px-4 ">{equipo.status}</span>
+
+                </div>
+                 ))}
+              </div>
+            </div>
+         </div>
       </div>
     </div>
   );
 };
-
+//chino el pro estuvo aqui
 export default Equipment;
